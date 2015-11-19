78fd48d22052a8cab07c86d2aebb806e334f8b54
mhuynh5757 <mhuynh5757@gmail.com>
2015-11-19 04:51:08 -0800

    made presentation stuff

--- HelpingHand.py
+++ HelpingHand.py
@@ -2,26 +2,27 @@ import serial, serial.tools.list_ports, Tkinter
 
 for device in list(serial.tools.list_ports.comports()):
     if 'Arduino' in device[1]:
-        arduino = serial.Serial(device[0], 115200, timeout=10)
+        arduino = serial.Serial(device[0], 115200, timeout=1)
         break
 
 root = Tkinter.Tk()
-
-header = Tkinter.Label(root, text='Helping Hand', font=('Courier', 16, 'bold'))
+root.title('g1yph softworks')
+2
+header = Tkinter.Label(root, text='Helping Hand', font=('Courier', 32, 'bold'))
 header.grid(row=0, column=0, columnspan=3)
 
-dist_label = Tkinter.Label(root, text='Distance: ', font=('Courier', 10))
+dist_label = Tkinter.Label(root, text='Distance: ', font=('Courier', 20))
 dist_label.grid(row=1, sticky=Tkinter.W)
-force_label = Tkinter.Label(root, text='Force: ', font=('Courier', 10))
+force_label = Tkinter.Label(root, text='Force: ', font=('Courier', 20))
 force_label.grid(row=2, sticky=Tkinter.W)
-temp_label = Tkinter.Label(root, text='Temperature: ', font=('Courier', 10))
+temp_label = Tkinter.Label(root, text='Temperature: ', font=('Courier', 20))
 temp_label.grid(row=3, sticky=Tkinter.W)
 
-dist_label_units = Tkinter.Label(root, text='cm', font=('Courier', 10))
+dist_label_units = Tkinter.Label(root, text='cm', font=('Courier', 20))
 dist_label_units.grid(row=1, column=2)
-force_label_units = Tkinter.Label(root, text='kg', font=('Courier', 10))
+force_label_units = Tkinter.Label(root, text='mN', font=('Courier', 20))
 force_label_units.grid(row=2, column=2)
-temp_label_units = Tkinter.Label(root, text='ºC', font=('Courier', 10))
+temp_label_units = Tkinter.Label(root, text='ºC', font=('Courier', 20))
 temp_label_units.grid(row=3, column=2)
 
 string_vars = []
@@ -31,7 +32,7 @@ for i in range(3):
     string_vars[i].set('     -')
     var_labels.append(Tkinter.Label(root,
                               textvariable=string_vars[i],
-                              font=('Courier', 10),
+                              font=('Courier', 20),
                               justify=Tkinter.RIGHT))
     var_labels[i].grid(row=i+1, column=1, sticky=Tkinter.E)
 
@@ -42,7 +43,7 @@ def update():
             for j in range(len(string_vars)):
                 string_vars[j].set(data[i].strip('$').split('!')[j].rjust(6, ' '))
             break
-    root.after(1, update)    
+    root.after(100, update)    
 update()
 
 root.mainloop()
--- HelpingHand/HelpingHand.ino
+++ HelpingHand/HelpingHand.ino
@@ -2,6 +2,9 @@ void setup()
 {
   Serial.begin(115200);
   Serial.print("Ready");
+  Serial.print("Ready");
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
 void loop()
@@ -9,7 +12,23 @@ void loop()
   Serial.print("$");
   Serial.print(12343.85 * pow(analogRead(A0), -1.15));
   Serial.print("!");
-  Serial.print(analogRead(A2));
+  int fsrReading = analogRead(A2);
+  int fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  unsigned long fsrResistance = 5000 - fsrVoltage;
+  fsrResistance *= 10000;
+  fsrResistance /= fsrVoltage;
+  unsigned long fsrConductance =  1000000;
+  fsrConductance /= fsrResistance;
+  long fsrForce = fsrConductance / .08;
+  Serial.print(fsrForce);
+  if(fsrForce > 500)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else
+  {
+    digitalWrite(13, LOW);
+  }
   Serial.print("!");
   Serial.print(((analogRead(A4) * .004882814) - .5) * 100);
   Serial.print("$");
